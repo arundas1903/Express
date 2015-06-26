0d19e252fe9299ad9e7295c108f10fad0bc9e871
arundas <arundas@qburst.com>
2015-06-26 15:49:53 +0530

    Trying sessions

--- ninja-store/routes/stores.js
+++ ninja-store/routes/stores.js
@@ -5,23 +5,14 @@ var mongo = require('mongoskin')
 var db = mongo.db('mongodb://localhost:27017/ninja_user');
 
 router.get('/', function(req, res, next){
-	res.render('home', {title: 'Ninja Store'})
-})
-
-router.post('/', function(req, res, next){
-	username = req.body.username || 'Anonymous';
     // store the username as a session variable
-    req.session.username = username;
-    db.collection('users').insert({username: username}, function(err, result){
-        if (err) throw err;
-        if (result) console.log('Added!');
-    });
-    db.collection('users').find().toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
+    if (req.session.username){
+        res.send("You are logged in")        
+    }
+    else{
+        res.send("Not logged in. Invalid permission")            
+    }
     // redirect the user to homepage
-    res.redirect('/');
 })
 
 module.exports = router;
--- ninja-store/routes/users.js
+++ ninja-store/routes/users.js
@@ -5,26 +5,27 @@ var mongo = require('mongoskin')
 var db = mongo.db('mongodb://localhost:27017/ninja_user');
 
 router.post('/sign_up', function(req, res, next) {
-    username = req.body.username
-    password1 = req.body.password1
-    password2 = req.body.password2
+    username = req.body.username;
+    password1 = req.body.password1;
+    password2 = req.body.password2;
     if (username && password1 && password2){
         if (password1 == password2) {
             existing_user = db.collection('users').find({username: username}).
             toArray(function(err, result) {
                 if (result.length){
-                    res.render('sign_up', {errors: 'Username already taken'})   
+                    res.render('sign_up', {errors: 'Username already taken'})   ;
                 }
                 else {
                     db.collection('users').insert({
                         username: username,
                         password: password1
-                    })
+                    });
+                    req.session.username = username;
                     res.redirect('/store/');  
             }});
         }
         else {
-            res.render('sign_up', {errors: 'Passwords did not match'})  
+            res.render('sign_up', {errors: 'Passwords did not match'});
         }
     }
     else{
@@ -38,7 +39,21 @@ router.get('/sign_up', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  res.redirect('/store/');
+    username = req.body.username
+    password = req.body.password
+    if (username && password){
+        user = db.collection('users').find({username: username, password: password}).
+        toArray(function(err, result) {
+            if (result.length){
+                res.render('login', {errors: 'Invalid username or password'})   
+            }
+            else {
+                res.redirect('/store/');  
+        }});
+    }
+    else{
+        res.render('login', {errors: 'Invalid username or password'})
+    }
 });
 
 router.get('/login', function(req, res, next) {
